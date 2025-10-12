0aa3cf76ef7471c5d0dbf7a96e024db881bacf57
Nicola <nicola.rigoni@madeinapp.net>
2025-10-12 12:53:20 +0200

    package creation

---
+++ example/flutter_athropic_interface_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_athropic_interface/flutter_athropic_interface.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/flutter_athropic_interface_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_athropic_interface/flutter_athropic_interface.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
